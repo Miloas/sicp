a42fe910b17bcb3afe80ac8d52a5f63345182f46
Miloas <0x9357@gmail.com>
2015-07-09 20:39:46 +0800

    add 1.8 solution

--- src/ch1.clj
+++ src/ch1.clj
@@ -1,5 +1,8 @@
 ;ch1 solutions
 
+;test fn
+(defn tt [x] (println x))
+
 ;1.2
 (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
             (* 3 (- 6 2) (- 2 7))) ; -47/150
@@ -8,7 +11,7 @@
 (defn sum-max2 [a b c]
   (- (+ a b c) (min a b c)))
 ;1.3 test
-(println (sum-max2 3 1 2))
+(tt (sum-max2 3 1 2))
 
 ;1.7
 (defn sqrt [x]
@@ -20,5 +23,16 @@
               (if (good-enough? guess new-guess) new-guess (help new-guess))))]
     (help 1.0)))
 ;1.7 test
-(println (sqrt 16))
+(tt (sqrt 16))
+
+;1.8
+(defn cube-root [x]
+  (letfn [(good-enough? [last-guess this-guess] (< (Math/abs (- last-guess this-guess)) 0.0000001))
+          (improve [guess] (/ (+ (/ x (* guess guess)) (* 2 guess))) 3)
+          (help [guess]
+            (let [new-guess (improve guess)]
+              (if (good-enough? guess new-guess) new-guess (help new-guess))))]
+    (help 1.0)))
+;1.8 test
+(tt (cube-root 27))
 
