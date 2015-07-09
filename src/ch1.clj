4218d1548deb55e4dba47249e5caf0cdd9856415
Miloas <0x9357@gmail.com>
2015-07-09 19:24:28 +0800

    first commit
    add 1.2 1.3 1.7 solution

---
+++ src/ch1.clj
@@ -0,0 +1,15 @@
+;ch1 solutions
+
+;1.2
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+            (* 3 (- 6 2) (- 2 7))) ; -47/150
+
+;1.3
+(defn sum-max2 [a b c]
+  (- (+ a b c) (min a b c)))
+;1.3 test
+(println (sum-max2 3 1 2))
+
+;1.7
+(defn sqrt [x]
+  ())
