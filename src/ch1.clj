f2fe90a48faf9c1201ccd304a24e1f2dd8bd14de
Miloas <0x9357@gmail.com>
2015-07-14 17:28:38 +0800

    add 1.35 solution

--- src/ch1.clj
+++ src/ch1.clj
@@ -318,3 +318,19 @@
       (= 17 (sum-prime-smaller 10))))
 (test-1-33)
 
+;1.35
+(def tolerance 0.00001)
+(defn fixed-point [f first-guess]
+  (letfn [(close-enough? [v1 v2]
+            (< (Math/abs (- v1 v2)) tolerance))
+          (try-guess [guess]
+            (let [next (f guess)]
+              (if (close-enough? guess next)
+                next
+                (try-guess next))))]
+    (try-guess first-guess)))
+(def phi (fixed-point (fn [x] (inc (/ 1 x))) 1.0))
+
+;1.36
+
+
