a7548df1521e3e1839b8ed815683536fd0344b54
Miloas <0x9357@gmail.com>
2015-07-15 19:03:39 +0800

    add 2.1 solution

--- src/ch2.clj
+++ src/ch2.clj
@@ -1,5 +1,20 @@
 ;ch2 solutions
+(ns sicp.ch2)
 
 ;test fn
 (defn tt [x] (println x))
 (use 'clojure.test)
+
+;2.1
+(defn gcd [a b]
+  (if (= b 0) a (gcd b (rem a b))))
+(defn make-rat [n d]
+  (let [v1 (Math/abs (gcd n d))
+        v2 (if (neg? d) (- v1) v1)]
+    [(/ n v2) (/ d v2)]))
+
+;2.1 test
+(deftest test-2-1
+  (is (= [-3 4] (make-rat -6 8))))
+(test-2-1)
+
