fb699387b57f34a2f5f53b5d937fbed4d391566b
Miloas <0x9357@gmail.com>
2015-07-15 19:39:02 +0800

    add 2.2 solution

--- src/ch2.clj
+++ src/ch2.clj
@@ -18,3 +18,36 @@
   (is (= [-3 4] (make-rat -6 8))))
 (test-2-1)
 
+;2.2
+(defn make-point [x y]
+  [x y])
+(defn get-x-coordinate [point]
+  (first point))
+(defn get-y-coordinate [point]
+  (second point))
+
+(defn make-segment [start end]
+  [start end])
+(defn get-start-point [segment]
+  (first segment))
+(defn get-end-point [segment]
+  (second segment))
+
+(defn print-point [point]
+  (println (format "(%d,%d)"
+             (get-x-coordinate point)
+             (get-y-coordinate point))))
+
+(defn midpoint-segment [segment]
+  (let [start-point (get-start-point segment)
+        end-point (get-end-point segment)
+        mid-x (/ (+ (get-x-coordinate start-point) (get-x-coordinate end-point)) 2)
+        mid-y (/ (+ (get-y-coordinate start-point) (get-y-coordinate end-point)) 2)]
+    (make-point mid-x mid-y)))
+
+;2.2 test
+(deftest test-2-2
+  (is (= [2 2] (midpoint-segment (make-segment [1 2] [3 2])))))
+(test-2-2)
+
+
