d941842dd198054b18439448e6f4f9f26de88142
Miloas <0x9357@gmail.com>
2015-07-29 02:14:12 +0800

    add 2.56 solution

--- src/ch2.clj
+++ src/ch2.clj
@@ -524,9 +524,57 @@
 (test-2-54)
 
 ;2.56
+(def variable? symbol?)
+(def same-variable? #(and (variable? %1)
+                          (variable? %2)
+                          (= %1 %2)))
+(defn =number? [exp num]
+  (and (number? exp)
+       (= exp num)))
 
+(def sum? #(and (list? %) (= (first %) '+)))
+(def addend second)
+(def augend #(nth % 2))
+(defn make-sum [a1 a2]
+  (cond (=number? a1 0) a2
+        (=number? a2 0) a1
+        (and (number? a1) (number? a2)) (+ a1 a2)
+        :else (list '+ a1 a2)))
+
+(def product? #(and (list? %) (= (first %) '*)))
+(def multipiler addend)
+(def multiplicand augend)
+(defn make-product [m1 m2]
+  (cond (or (=number? m1 0) (=number? m2 0)) 0
+        (and (number? m1) (product? m2)) (make-product (* m1 (multipiler m2)) (multiplicand m2))
+        (and (product? m1) (number? m2)) (make-product (* (multipiler m1) m2) (multiplicand m1))
+        (=number? m1 1) m2
+        (=number? m2 1) m1
+        (and (number? m1) (number? m2)) (* m1 m2)
+        :else (list '* m1 m2)))
+
+(def exponentiation? #(and (list? %) (= (first %) '**)))
+(def base addend)
+(def exponent augend)
+(defn make-exponentiation [b n]
+  (cond (=number? n 1) b
+        (=number? n 0) 0
+        :else (list '** b n)))
+
+(defn deriv [exp var]
+  (cond (number? exp) 0
+        (variable? exp) (if (same-variable? exp var) 1 0)
+        (sum? exp) (make-sum (deriv (addend exp) var) (deriv (augend exp) var))
+        (product? exp) (make-sum (make-product (multipiler exp) (deriv (multiplicand exp) var))
+                     (make-product (deriv (multipiler exp) var) (multiplicand exp)))
+        (exponentiation? exp) (make-product (exponent exp)
+                                (make-product (make-exponentiation (base exp) (dec (exponent exp)))
+                                  (deriv (base exp) var)))
+    :else (throw (Exception. (format "unknow expression type %s DERUV" exp)))))
 
 ;2.56 test
-
+(deftest test-2-56
+  (is (= '(* 9 (** x 2)) (deriv '(* 3 (** x 3)) 'x))))
+(test-2-56)
 
 
