029644bc85bf26a974fadc397f5b6ed7a6470462
Miloas <0x9357@gmail.com>
2015-08-01 11:20:31 +0800

    add 3.1 solution

--- src/ch3.clj
+++ src/ch3.clj
@@ -7,4 +7,21 @@
 
 (def balance (atom 100))
 (defn withdraw [amount]
-  (if))
+  (if (>= @balance amount)
+      ;(reset! balance (- @balance amount))
+      (swap! balance #(- % amount))
+    (throw (RuntimeException. "Insufficient funds."))))
+
+;3.1
+(defn make-accumulator [initial]
+  (let [acc (atom initial)]
+    (fn [value]
+      (swap! acc #(+ % value)))))
+
+;3.1 test
+(deftest test-3-1
+  (let [A (make-accumulator 5)]
+    (is (= 15 (A 10)))
+    (is (= 25 (A 10)))))
+(test-3-1)
+
