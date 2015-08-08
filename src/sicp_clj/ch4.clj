057176e443e69c98caa50a4d2156ff0164d5523c
Miloas <0x9357@gmail.com>
2015-08-08 22:35:56 +0800

    add  4.1 solution

--- src/sicp_clj/ch4.clj
+++ src/sicp_clj/ch4.clj
@@ -3,4 +3,21 @@
 ;test fn
 (def tt println)
 
+;4.1
+(declare no-operands?)
+(declare first-operand)
+(declare eval')
+(declare rest-operands)
+;left->right
+(defn list-of-values' [exps env]
+  (if (no-operands? exps)
+    '()
+    (let [x (eval' (first-operand exps) env)]
+      (cons x (list-of-values' (rest-operands exps) env)))))
+;right->left
+(defn list-of-values'' [exps env]
+  (if (no-operands? exps)
+    '()
+    (let [x (list-of-values'' (rest-operands exps) env)]
+      (cons (eval' (first-operand exps) env) x))))
 
